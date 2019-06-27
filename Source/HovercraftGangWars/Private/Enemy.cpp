69de7b36c0f9f524e88524c8595b1e94a455a3c3
Ali <elsaleh78@gmail.com>
2019-06-27 12:06:54 +1000

    Fix an issue where the mesh was not set to zero vector for location

--- Source/HovercraftGangWars/Private/Enemy.cpp
+++ Source/HovercraftGangWars/Private/Enemy.cpp
@@ -11,11 +11,11 @@ AEnemy::AEnemy()
 
 	// Scene component
 	SceneComponent = CreateDefaultSubobject<USceneComponent>(FName("Scene Component"));
-	SceneComponent = RootComponent;
+	RootComponent = SceneComponent;
 
 	// Static mesh component
 	StaticMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(FName("StaticMeshComponent"));
-	StaticMeshComponent->AttachToComponent(RootComponent, FAttachmentTransformRules::KeepRelativeTransform);
+	StaticMeshComponent->SetupAttachment(RootComponent);
 
 	StaticMesh = Cast<UStaticMesh>(StaticLoadObject(UStaticMesh::StaticClass(), nullptr, TEXT("StaticMesh'/Game/Geometry/Meshes/1M_Cube.1M_Cube'")));
 
@@ -24,26 +24,16 @@ AEnemy::AEnemy()
 		StaticMeshComponent->SetStaticMesh(StaticMesh);
 	}
 
-	StaticMeshComponent->SetWorldLocation(GetActorLocation());
+	StaticMeshComponent->SetRelativeLocation(FVector(0.0f));
 	StaticMeshComponent->SetWorldScale3D(FVector(0.5f, 0.5f, 1.0f));
 }
 
 void AEnemy::BeginPlay()
 {
 	Super::BeginPlay();
-
-	StaticMeshComponent->SetWorldLocation(GetActorLocation());
 }
 
 void AEnemy::Tick(const float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 }
-
-void AEnemy::PostEditMove(const bool bFinished)
-{
-	Super::PostEditMove(bFinished);
-
-	//StaticMeshComponent->SetRelativeLocation(FVector(0.0f));
-}
-
